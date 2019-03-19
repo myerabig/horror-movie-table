4b5282912e889fff497b18dab4132062d0418c53
myerabig <myerabig@aol.com>
2019-03-19 14:25:25 -0500

    .

--- mainpage.js
+++ mainpage.js
@@ -1,8 +1,8 @@
-function changetoTitle(){
-    document.getElementById("titleSearchBar").style = "display: block";
-    document.getElementById("yearSearchBar").style = "display: none";
-    document.getElementById("directorSearchBar").style = "display: none";
-    document.getElementById("actorSearchBar").style = "display: none";
+function changeToTitle(){
+    document.getElementById("titleSearchbar").style = "display: block";
+    document.getElementById("yearSearchbar").style = "display: none";
+    document.getElementById("directorSearchbar").style = "display: none";
+    document.getElementById("actorSearchbar").style = "display: none";
 
     document.getElementById("titleOption").style = "color: white";
     document.getElementById("yearOption").style = "color: orange";
@@ -10,11 +10,11 @@ function changetoTitle(){
     document.getElementById("actorOption").style = "color: orange";
 }
 
-function changetoYear(){
-    document.getElementById("titleSearchBar").style = "display: none";
-    document.getElementById("yearSearchBar").style = "display: block";
-    document.getElementById("directorSearchBar").style = "display: none";
-    document.getElementById("actorSearchBar").style = "display: none";
+function changeToYear(){
+    document.getElementById("titleSearchbar").style = "display: none";
+    document.getElementById("yearSearchbar").style = "display: block";
+    document.getElementById("directorSearchbar").style = "display: none";
+    document.getElementById("actorSearchbar").style = "display: none";
 
     document.getElementById("titleOption").style = "color: orange";
     document.getElementById("yearOption").style = "color: white";
@@ -22,11 +22,11 @@ function changetoYear(){
     document.getElementById("actorOption").style = "color: orange";
 }
 
-function changetoDirector(){
-    document.getElementById("titleSearchBar").style = "display: none";
-    document.getElementById("yearSearchBar").style = "display: none";
-    document.getElementById("directorSearchBar").style = "display: block";
-    document.getElementById("actorSearchBar").style = "display: none";
+function changeToDirector(){
+    document.getElementById("titleSearchbar").style = "display: none";
+    document.getElementById("yearSearchbar").style = "display: none";
+    document.getElementById("directorSearchbar").style = "display: block";
+    document.getElementById("actorSearchbar").style = "display: none";
 
     document.getElementById("titleOption").style = "color: orange";
     document.getElementById("yearOption").style = "color: orange";
@@ -34,11 +34,11 @@ function changetoDirector(){
     document.getElementById("actorOption").style = "color: orange";
 }
 
-function changetoActor(){
-    document.getElementById("titleSearchBar").style = "display: none";
-    document.getElementById("yearSearchBar").style = "display: none";
-    document.getElementById("directorSearchBar").style = "display: none";
-    document.getElementById("actorSearchBar").style = "display: block";
+function changeToActor(){
+    document.getElementById("titleSearchbar").style = "display: none";
+    document.getElementById("yearSearchbar").style = "display: none";
+    document.getElementById("directorSearchbar").style = "display: none";
+    document.getElementById("actorSearchbar").style = "display: block";
 
     document.getElementById("titleOption").style = "color: orange";
     document.getElementById("yearOption").style = "color: orange";
@@ -48,7 +48,7 @@ function changetoActor(){
 
 function titleSearch(){
     var movies = "";
-    var search = document.getElementById("searchbar").value;
+    var search = document.getElementById("titleSearchbar").value;
     var titleName = "";
     var searchHeader = "<tr><th width=\"14%\">Title</th><th width=\"3%\">Release Year</th><th width=\"10%\">Director</th><th width=\"2%\">IMDB Rating/10</th><th width=\"10%\">Subgenre</th><th width=\"15%\">Lead Actor(s)</th><th width=\"45%\">Plot Summary</th><th width=\"2%\">Runtime (minutes)</th></tr>";
 
@@ -64,7 +64,33 @@ function titleSearch(){
         }
     }
 
-    if(document.getElementById("searchbar").value == ""){
+    if(document.getElementById("titleSearchbar").value == ""){
+        document.getElementById("movieChart").style = "display: block";
+        document.getElementById("searchTable").style = "display: none";
+    }
+
+    document.getElementById("searchTable").innerHTML = searchHeader + movies;
+}
+
+function yearSearch(){
+    var movies = "";
+    var search = document.getElementById("yearSearchbar").value;
+    var yearName = "";
+    var searchHeader = "<tr><th width=\"14%\">Title</th><th width=\"3%\">Release Year</th><th width=\"10%\">Director</th><th width=\"2%\">IMDB Rating/10</th><th width=\"10%\">Subgenre</th><th width=\"15%\">Lead Actor(s)</th><th width=\"45%\">Plot Summary</th><th width=\"2%\">Runtime (minutes)</th></tr>";
+
+    document.getElementById("movieChart").style = "display: none";
+    document.getElementById("searchTable").style = "display: block";
+    document.getElementById("searchTable").innerHTML = searchHeader;
+
+    for (let i = 0; i < document.getElementsByClassName("row").length; i++){
+        yearName = document.getElementsByClassName("row")[i].getElementsByClassName("releaseYear")[0].innerHTML;
+
+        if(yearName.indexOf(search) != -1){
+            movies += document.getElementsByClassName("row")[i].outerHTML;
+        }
+    }
+
+    if(document.getElementById("yearSearchbar").value == ""){
         document.getElementById("movieChart").style = "display: block";
         document.getElementById("searchTable").style = "display: none";
     }
