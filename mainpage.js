03f36fe551b032c2f345e42cce7fa44e852858ac
myerabig <myerabig@aol.com>
2019-03-19 15:18:58 -0500

    .

--- mainpage.js
+++ mainpage.js
@@ -97,3 +97,55 @@ function yearSearch(){
 
     document.getElementById("searchTable").innerHTML = searchHeader + movies;
 }
+
+function directorSearch(){
+    var movies = "";
+    var search = document.getElementById("directorSearchbar").value;
+    var directorName = "";
+    var searchHeader = "<tr><th width=\"14%\">Title</th><th width=\"3%\">Release Year</th><th width=\"10%\">Director</th><th width=\"2%\">IMDB Rating/10</th><th width=\"10%\">Subgenre</th><th width=\"15%\">Lead Actor(s)</th><th width=\"45%\">Plot Summary</th><th width=\"2%\">Runtime (minutes)</th></tr>";
+
+    document.getElementById("movieChart").style = "display: none";
+    document.getElementById("searchTable").style = "display: block";
+    document.getElementById("searchTable").innerHTML = searchHeader;
+
+    for (let i = 0; i < document.getElementsByClassName("row").length; i++){
+        directorName = document.getElementsByClassName("row")[i].getElementsByClassName("director")[0].innerHTML;
+
+        if(directorName.indexOf(search) != -1){
+            movies += document.getElementsByClassName("row")[i].outerHTML;
+        }
+    }
+
+    if(document.getElementById("directorSearchbar").value == ""){
+        document.getElementById("movieChart").style = "display: block";
+        document.getElementById("searchTable").style = "display: none";
+    }
+
+    document.getElementById("searchTable").innerHTML = searchHeader + movies;
+}
+
+function actorSearch(){
+    var movies = "";
+    var search = document.getElementById("actorSearchbar").value;
+    var actorName = "";
+    var searchHeader = "<tr><th width=\"14%\">Title</th><th width=\"3%\">Release Year</th><th width=\"10%\">Director</th><th width=\"2%\">IMDB Rating/10</th><th width=\"10%\">Subgenre</th><th width=\"15%\">Lead Actor(s)</th><th width=\"45%\">Plot Summary</th><th width=\"2%\">Runtime (minutes)</th></tr>";
+
+    document.getElementById("movieChart").style = "display: none";
+    document.getElementById("searchTable").style = "display: block";
+    document.getElementById("searchTable").innerHTML = searchHeader;
+
+    for (let i = 0; i < document.getElementsByClassName("row").length; i++){
+        actorName = document.getElementsByClassName("row")[i].getElementsByClassName("actors")[0].innerHTML;
+
+        if(actorName.indexOf(search) != -1){
+            movies += document.getElementsByClassName("row")[i].outerHTML;
+        }
+    }
+
+    if(document.getElementById("actorSearchbar").value == ""){
+        document.getElementById("movieChart").style = "display: block";
+        document.getElementById("searchTable").style = "display: none";
+    }
+
+    document.getElementById("searchTable").innerHTML = searchHeader + movies;
+}
